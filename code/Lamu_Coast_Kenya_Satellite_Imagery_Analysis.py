d4b60e5626f094d8a1cbabcc4b8fe16eb2cb42e1
Okumu-jr <alexokumu006@gmail.com>
2023-09-14 10:57:19 +0300

    Analysis

---
+++ code/Lamu_Coast_Kenya_Satellite_Imagery_Analysis.py
@@ -0,0 +1,150 @@
+from glob import glob
+
+import earthpy as et
+import earthpy.spatial as es
+import earthpy.plot as ep
+
+import rasterio as rio
+from rasterio.plot import plotting_extent
+from rasterio.plot import show
+from rasterio.plot import reshape_as_raster, reshape_as_image
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import ListedColormap
+
+import plotly.graph_objects as go
+
+np.seterr(divide='ignore', invalid='ignore')
+
+#DATA SETS
+#Lamu coast data
+
+S_sentinel_bands = glob("D:\Final Project\Satellite_\Data\Lamu_Coast_Kenya\*B?*.tif")
+S_sentinel_bands.sort()
+S_sentinel_bands
+
+l = []
+for i in S_sentinel_bands:
+  with rio.open(i, 'r') as f:
+    l.append(f.read(1))
+    arr_st = np.stack(l)
+    arr_st.shape
+
+#RGB Composite image    
+ep.plot_bands(arr_st, cmap = 'gist_earth', figsize = (20, 12), cols = 6, cbar = False)
+plt.show()
+
+rgb = ep.plot_rgb(arr_st, 
+                  rgb=(3,2,1), 
+                  figsize=(10, 16), 
+                  # title='RGB Composite Image'
+                  )
+
+plt.show()
+
+ep.plot_rgb(
+    arr_st,
+    rgb=(3, 2, 1),
+    stretch=True,
+    str_clip=0.2,
+    figsize=(10, 16),
+    # title="RGB Composite Image with Stretch Applied",
+)
+
+plt.show()
+
+colors = ['tomato', 'navy', 'MediumSpringGreen', 'lightblue', 'orange', 'blue',
+          'maroon', 'purple', 'yellow', 'olive', 'brown', 'cyan']
+
+ep.hist(arr_st, 
+         colors = colors,
+        title=[f'Band-{i}' for i in range(1, 13)], 
+        cols=3, 
+        alpha=0.5, 
+        figsize = (12, 10)
+        )
+
+plt.show()
+
+#VEGETATION AND SOIL INDICES
+#Normalized Difference Vegetation index (NDVI)
+
+#NDVI = ((NIR - Red)/(NIR + Red))
+#where: NIR is Pixel values from the near-infrared band
+#       Red is Pixel values from the red band
+
+ndvi = es.normalized_diff(arr_st[7], arr_st[3])
+
+ep.plot_bands(ndvi, cmap="RdYlGn", cols=1, vmin=-1, vmax=1, figsize=(10, 14))
+
+plt.show()
+
+#Soil-Adjusted Vegetation index  (SAVI)
+#SAVI = ((NIR - Red)/(NIR + Red + L)) * (1 + L)
+#where: L is the amount of green vegetation cover
+
+L = 0.5
+
+savi = ((arr_st[7] - arr_st[3]) / (arr_st[7] + arr_st[3] + L)) * (1 + L)
+
+ep.plot_bands(savi, cmap="RdYlGn", cols=1, vmin=-1, vmax=1, figsize=(10, 14))
+
+plt.show()
+
+#Visible Atmospherically Resistant Index (VARI)
+#VARI = (Green - Red)/(Green + Red - Blue)
+#where: Green is Pixel values from the green band
+#       Blue  is pixel values from the blue band
+
+vari = (arr_st[2] - arr_st[3])/ (arr_st[2] + arr_st[3] - arr_st[1])
+ep.plot_bands(vari, cmap="RdYlGn", cols=1, vmin=-1, vmax=1, figsize=(10, 14))
+
+plt.show()
+
+#Distribution of NVDI,SAVI and VARI Pixel values
+ep.hist(np.stack([ndvi, savi, vari]), 
+        alpha=0.5,
+        cols=3, 
+        figsize=(20, 5),
+        title = ['NDVI', 'SAVI', 'VARI'],
+        colors = ['mediumspringgreen', 'tomato', 'navy'])
+plt.show()
+
+#Water Indices
+#Modified Normalized Difference Water Index (MNDWI)
+#MNDWI = (Green - SWIR)/(Green + SWIR)
+#where: SWIR is pixel value from the short-wave infrared band
+
+mndwi = es.normalized_diff(arr_st[2], arr_st[10])
+ep.plot_bands(mndwi, cmap="RdYlGn", cols=1, vmin=-1, vmax=1, figsize=(10, 14))
+
+plt.show()
+
+#Normalized Difference Moisture Index(NDMI)
+#NDMI = (NIR - SWIR1)/(NIR + SWIR1)
+#where: SWIR1 is pixel value from the short-wave infrared 1 band
+
+ndmi = es.normalized_diff(arr_st[7], arr_st[10])
+ep.plot_bands(ndmi, cmap="RdYlGn", cols=1, vmin=-1, vmax=1, figsize=(10, 14))
+
+plt.show()
+
+
+#Geology Indices
+#Clay Minerals
+#Clay Minerals Ratio = SWIR1 / SWIR2
+#where: SWIR2 is pixel values from the short-wave onfrared 2 band
+
+cmr = np.divide(arr_st[10], arr_st[11])
+ep.plot_bands(cmr, cmap="RdYlGn", cols=1, vmin=-1, vmax=1, figsize=(10, 14))
+
+plt.show()
+
+#Ferrous Minerals
+#Ferrous Minerals Ratio = SWIR / NIR
+
+fmr = np.divide(arr_st[10], arr_st[7])
+ep.plot_bands(fmr, cmap="RdYlGn", cols=1, vmin=-1, vmax=1, figsize=(10, 14))
+
+plt.show()
---
+++ code/Unsupervised_learning_in_Satellite_imagery.py
@@ -0,0 +1,126 @@
+from glob import glob
+
+import earthpy as et
+import earthpy.spatial as es
+import earthpy.plot as ep
+
+import rasterio as rio
+from rasterio.plot import plotting_extent
+from rasterio.plot import show
+from rasterio.plot import reshape_as_raster, reshape_as_image
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.colors import ListedColormap
+
+import plotly.graph_objects as go
+
+np.seterr(divide='ignore', invalid='ignore')
+
+#reading the data
+
+S_sentinel_bands = glob("D:\Final Project\Satellite_\Data\Lamu_Coast_Kenya\*B?*.tif")
+S_sentinel_bands.sort()
+S_sentinel_bands
+
+l = []
+for i in S_sentinel_bands:
+  with rio.open(i, 'r') as f:
+    l.append(f.read(1))
+
+arr_st = np.stack(l)
+
+print(f'Height: {arr_st.shape[1]}\nWidth: {arr_st.shape[2]}\nBands: {arr_st.shape[0]}')
+
+#Visualize data
+
+ep.plot_bands(arr_st, cmap = 'gist_earth', figsize = (20, 12), cols = 6, cbar = False)
+plt.show()
+
+#RGB Composite
+
+rgb = ep.plot_rgb(arr_st, 
+                  rgb=(3,2,1), 
+                  figsize=(8, 10), 
+                  # title='RGB Composite Image'
+                  )
+
+plt.show()
+
+ep.plot_rgb(
+    arr_st,
+    rgb=(3, 2, 1),
+    stretch=True,
+    str_clip=0.2,
+    figsize=(8, 10),
+    # title="RGB Composite Image with Stretch Applied",
+)
+
+plt.show()
+
+#Data distribution of bands
+
+colors = ['tomato', 'navy', 'MediumSpringGreen', 'lightblue', 'orange', 'blue',
+          'maroon', 'purple', 'yellow', 'olive', 'brown', 'cyan']
+
+ep.hist(arr_st, 
+         colors = colors,
+        title=[f'Band-{i}' for i in range(1, 13)], 
+        cols=3, 
+        alpha=0.5, 
+        figsize = (12, 10)
+        )
+
+plt.show()
+
+#Preprocessing
+
+x = np.moveaxis(arr_st, 0, -1)
+x.shape
+x.reshape(-1, 12).shape, 954*298
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+X_data = x.reshape(-1, 12)
+
+scaler = StandardScaler().fit(X_data)
+
+X_scaled = scaler.transform(X_data)
+
+X_scaled.shape
+
+#Principle Component Analysis
+
+pca = PCA(n_components = 4)
+
+pca.fit(X_scaled)
+
+data = pca.transform(X_scaled)
+data.shape
+pca.explained_variance_ratio_
+np.sum(pca.explained_variance_ratio_)
+
+#Visualize Data after PCA
+
+ep.plot_bands(np.moveaxis(data.reshape((954, 298, data.shape[1])), -1, 0),
+              cmap = 'gist_earth',
+              cols = 4,
+              title = [f'PC-{i}' for i in range(1,5)])
+
+plt.show()
+
+#k-means
+
+from sklearn.cluster import KMeans
+
+kmeans = KMeans(n_clusters = 6, random_state = 11)
+
+kmeans.fit(data)
+labels = kmeans.predict(data)
+np.unique(labels)
+
+#Visualize Clusters
+ep.plot_bands(labels.reshape(954, 298), cmap=ListedColormap(['darkgreen', 'green', 'black', '#CA6F1E', 'navy', 'forestgreen']))
+plt.show()
+
